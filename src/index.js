b7b5a2f6a5594c92e938cf119583f60a6691df53
10SPECTOR <goudkritesh@gmail.com>
2026-06-25 17:38:09 +0530

    use dotenv environment variables for database configuration

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require("express");
 
 const tasksRouter = require('./routes/tasks');
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/tasks", tasksRouter);
